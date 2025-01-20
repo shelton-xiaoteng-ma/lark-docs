5837e30131272d2632c8bb86e027f2fbfbda262d
maxiaoteng <jizhuwo00@hotmail.com>
2025-01-20 18:58:03 +0800

    fix(middleware): update matcher to exclude signIn/signUp routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
 export { auth as middleware } from "@/auth";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|templates|sign-in|sign-up).*)",
+  ],
 };
