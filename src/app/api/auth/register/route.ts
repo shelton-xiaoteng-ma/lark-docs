5fb49ab65ca3da1c0fc3355364e07fc78f22b6c1
maxiaoteng <jizhuwo00@hotmail.com>
2025-01-20 22:10:39 +0800

    refactor(auth/register): update registration logic

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -2,36 +2,34 @@ import { db } from "@/db/drizzle";
 import { users } from "@/db/schema";
 import { getUserFromDb } from "@/lib/db";
 import { saltAndHashPassword } from "@/lib/password";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const { email, password } = await req.json();
-
     const existingUser = await getUserFromDb(email);
 
-    if (!existingUser) {
+    if (existingUser) {
       return NextResponse.json(
-        { message: "This email address has been registered" },
+        {
+          message: "This email address has been registered",
+        },
         { status: 400 }
       );
     }
 
     const hashedPassword = saltAndHashPassword(password);
-
     await db.insert(users).values({
       email,
       password: hashedPassword,
     });
 
-    return new Response(
-      JSON.stringify({ message: "Register success", user: { email } }),
+    return NextResponse.json(
+      { message: "Register success", user: { email } },
       { status: 201 }
     );
   } catch (error) {
     console.error("register error:", error);
-    return new Response(JSON.stringify({ message: "register error" }), {
-      status: 500,
-    });
+    return NextResponse.json({ message: "register error" }, { status: 500 });
   }
 }
